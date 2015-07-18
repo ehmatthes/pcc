335d6c106e0da6e9cdaa82a3313a5c0cf4337b51
Eric Matthes <ehmatthes@gmail.com>
2015-07-17 22:14:14 -0800

    Updated chapter 15 screenshots.

--- chapter_15/die_visual.py
+++ chapter_15/die_visual.py
@@ -20,7 +20,7 @@ for value in range(1, die.num_sides+1):
 # Visualize the results.
 hist = pygal.Bar()
 
-hist.title = "Results of rolling one D6 1,000 times."
+hist.title = "Results of rolling one D6 1000 times."
 hist.x_labels = ['1', '2', '3', '4', '5', '6']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
