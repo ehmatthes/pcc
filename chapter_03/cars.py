2cacf524bcca70088a5a7084464adb84e53c1615
Eric Matthes <ehmatthes@gmail.com>
2015-09-26 21:22:02 -0800

    Added rest of chapter 3 code.

---
+++ chapter_03/cars.py
@@ -0,0 +1,13 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the reverse alphabetical list:")
+print(sorted(cars, reverse=True))
+
+print("\nHere is the original list again:")
+print(cars)
