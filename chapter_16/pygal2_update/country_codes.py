b6e93516cfb3fdc86294748011e4830fe9848221
Eric Matthes <ehmatthes@gmail.com>
2016-02-28 17:29:45 -0900

    Copied files that need updating to new directory.

---
+++ chapter_16/pygal2_update/country_codes.py
@@ -0,0 +1,9 @@
+from pygal.i18n import COUNTRIES
+
+def get_country_code(country_name):
+    """Return the Pygal 2-digit country code for the given country."""
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+    # If the country wasn't found, return None.
+    return None
