6753127e31cc00aa2bcd3c12e3f36da06fb6058d
macroghan <macroghan@gmail.com>
2016-08-04 13:22:17 -0400

    Update countries.py
    I didn't include '.i18n' in my original edit.

--- chapter_16/pygal2_update/countries.py
+++ chapter_16/pygal2_update/countries.py
@@ -1,4 +1,4 @@
-from pygal_maps_world import COUNTRIES
+from pygal_maps_world.i18n import COUNTRIES
 
 for country_code in sorted(COUNTRIES.keys()):
     print(country_code, COUNTRIES[country_code])
