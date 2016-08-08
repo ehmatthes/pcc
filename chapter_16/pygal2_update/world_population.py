d94d8ec374a97e2588326df03d3c531689eca88b
Matt Croghan <macroghan@gmail.com>
2016-08-08 13:29:48 -0400

    Update world_population.py

--- chapter_16/pygal2_update/world_population.py
+++ chapter_16/pygal2_update/world_population.py
@@ -1,6 +1,6 @@
 import json
 
-from pygal.maps.world import World
+from pygal_maps_world.maps import World
 from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
 from country_codes import get_country_code
