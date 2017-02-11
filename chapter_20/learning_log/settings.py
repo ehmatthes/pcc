53b5690163fb7bbae1be18725c764cb7b13b812f
Eric Matthes <ehmatthes@gmail.com>
2017-02-11 12:00:02 -0900

    Updated python_repos.py, settings.py to match version in latest printing.

--- chapter_20/learning_log/settings.py
+++ chapter_20/learning_log/settings.py
@@ -117,7 +117,8 @@ BOOTSTRAP3 = {
     }
 
 # Heroku settings
-if os.getcwd() == '/app':
+cwd = os.getcwd()
+if cwd == '/app' or cwd[:4] == '/tmp':
     import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost')
